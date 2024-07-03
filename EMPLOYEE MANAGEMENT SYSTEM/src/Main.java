8ecd68e302d7b0662b62bedecfa31f2200b4d7f5
linnypham <89488749+linnypham@users.noreply.github.com>
2024-07-03 17:00:29 -0400

    databse stuff

--- EMPLOYEE MANAGEMENT SYSTEM/src/Main.java
+++ EMPLOYEE MANAGEMENT SYSTEM/src/Main.java
@@ -5,7 +5,7 @@ import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root","password123456");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/employeedb", "root","password123456");
 
         Statement statement = connection.createStatement();
 
