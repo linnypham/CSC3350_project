013df563163728fe055d27ffe8b34b7ecf93cee3
linnypham <89488749+linnypham@users.noreply.github.com>
2024-07-03 12:24:58 -0400

    basic

--- EMPLOYEE MANAGEMENT SYSTEM/src/Main.java
+++ EMPLOYEE MANAGEMENT SYSTEM/src/Main.java
@@ -1,6 +1,15 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Main {
     public static void main(String[] args) {
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root","password123456");
+
+        Statement statement = connection.createStatement():
+
+        ResultSet resultSet = statement.executeQuery():
 
 
     }
--- EMPLOYEE MANAGEMENT SYSTEM/src/table.java
+++ EMPLOYEE MANAGEMENT SYSTEM/src/table.java
@@ -1,5 +1,5 @@
 public interface table {
-    public String updateTable();
     public String addColumn();
     public String searchTable();
+    public String updateTable();
 }
