7196f5e05f365e31ec22e188f44613da54168997
linnypham <89488749+linnypham@users.noreply.github.com>
2024-07-06 14:03:59 -0400

    table
    be able to output table

--- EMPLOYEE MANAGEMENT SYSTEM/src/Main.java
+++ EMPLOYEE MANAGEMENT SYSTEM/src/Main.java
@@ -1,23 +1,36 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
-        try{
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/employeedb", "root","password123456");
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/employeedb", "root", "password123456");
 
             Statement statement = connection.createStatement();
 
-            ResultSet resultSet = statement.executeQuery("select * from employee;");
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM employee;");
 
-            while (resultSet.next()){
-                System.out.println(resultSet.getString("fname"));
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            int columnCount = metaData.getColumnCount();
+
+            // Print column names
+            for (int i = 1; i <= columnCount; i++) {
+                System.out.print(metaData.getColumnName(i) + "\t");
             }
-        }catch (Exception e){
+            System.out.println();
+
+            // Print row data
+            while (resultSet.next()) {
+                for (int i = 1; i <= columnCount; i++) {
+                    System.out.print(resultSet.getString(i) + "\t");
+                }
+                System.out.println();
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 }
