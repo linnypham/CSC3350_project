73f62ce8faa0d459b0bdb38b7b20c607c08f71fd
linnypham <89488749+linnypham@users.noreply.github.com>
2024-07-06 21:46:56 -0400

    JavaFx

---
+++ JavaFX/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.javafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.javafx to javafx.fxml;
+    exports com.example.javafx;
+}
