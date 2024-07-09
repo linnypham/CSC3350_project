3c8c5ee8ef3c88d39242f90b4f962e456d684146
Marieezouaa <88006867+Marieezouaa@users.noreply.github.com>
2024-07-09 15:41:55 -0400

    Update DatabaseConnection.java

--- EMPLOYEE MANAGEMENT SYSTEM/src/DatabaseConnection.java
+++ EMPLOYEE MANAGEMENT SYSTEM/src/DatabaseConnection.java
@@ -1,13 +1,20 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
+import java.sql.*;
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/employeedb";
-    private static final String USER = "root";
-    private static final String PASSWORD = "password123456";
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+    private Connection connection;
+
+    void createConnection() { //creates connection
+        try {
+             connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/employeedb",
+                    "root",
+                    "password");
+        }
+        catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public Connection getConnection() {
+        return connection;
     }
 }
