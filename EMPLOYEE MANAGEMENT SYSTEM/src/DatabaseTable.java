fcfba599bd8001a1eab05a948cc13b6a5487c0dd
Marieezouaa <88006867+Marieezouaa@users.noreply.github.com>
2024-07-09 14:12:15 -0400

    Add files via upload
    Functionality for interacting with the database

---
+++ EMPLOYEE MANAGEMENT SYSTEM/src/DatabaseTable.java
@@ -0,0 +1,138 @@
+import java.sql.*;
+
+public class DatabaseTable implements DatabaseFunctionality {
+
+    String tableName;
+    DatabaseConnection dbConnection;
+    Connection connection;
+
+    //constructor
+    DatabaseTable() throws SQLException {
+        super();
+        this.tableName = tableName;
+        dbConnection = new DatabaseConnection(); //obj of DatabaseConnection class
+        dbConnection.createConnection(); //establishing connection to database
+        connection = dbConnection.getConnection(); //returns connection
+    }
+
+    //methods
+    @Override
+    public void createTable(String tableName) throws SQLException {
+
+        if(connection != null){//check for connection first
+
+            if(!tableExist(connection, tableName)){//if the table does not exist
+                try {
+                    Statement statement = connection.createStatement();
+                    String createTable = "CREATE TABLE " +tableName+" ("+
+                            "empId INT PRIMARY KEY,"+
+                            "firstName VARCHAR(25),"+
+                            "lastName VARCHAR(25),"+
+                            "division VARCHAR(50),"+
+                            "jobTitle VARCHAR(50),"+
+                            "salary DECIMAL(10,2),"+
+                            "ssn CHAR(9) )";
+                    statement.execute(createTable);
+                    System.out.println("Table " +tableName+" created");
+
+                }catch(SQLException e) {
+                    e.printStackTrace();
+                }
+            }else{
+                System.out.println("Table " +tableName+ " already exist");
+            }
+        }
+    }
+
+    //checks if tableExist
+    public boolean tableExist( Connection connection, String tableName) throws SQLException {
+        boolean exist = false;
+
+        try (ResultSet resultSet = connection.getMetaData().getTables(null, null, tableName, null)) {
+            while (resultSet.next()) {
+                String tName = resultSet.getString("TABLE_NAME");
+                if (tName != null && tName.equals(tableName)) {
+                    exist = true;
+                    break;
+                }
+            }
+        }
+
+        return exist;
+    }
+
+    //checks if column exist
+    public boolean columnExist(String tableName, String columnName) throws SQLException{
+        boolean exist = false;
+
+        try (ResultSet resultSet = connection.getMetaData().getColumns(null, null, columnName, null)){
+            while(resultSet.next()){
+                String cName = resultSet.getString("COLUMN_NAME");
+                if (cName != null && cName.equals(columnName)) {
+                    exist = true;
+                    break;
+                }
+            }
+        }
+        return exist;
+    }
+
+    //updates table
+    public void updateTable() throws SQLException{//vague
+        if(connection != null){//check for connection first
+
+            if(!tableExist(connection, tableName)){//if the table does not exist
+                try {
+                    Statement statement = connection.createStatement();
+                    String updateTable = "CREATE TABLE " +tableName+" ("+
+                            "empId INT PRIMARY KEY,"+
+                            "firstName VARCHAR(25),"+
+                            "lastName VARCHAR(25),"+
+                            "division VARCHAR(50),"+
+                            "jobTitle VARCHAR(50),"+
+                            "salary DECIMAL(10,2),"+
+                            "ssn CHAR(9) )";
+                    statement.execute(updateTable);
+                    System.out.println("Table " +tableName+" created");
+
+                }catch(SQLException e) {
+                    e.printStackTrace();
+                }
+            }else{
+                System.out.println("Table " +tableName+ " already exist");
+            }
+        }
+
+    }
+
+    //allows for searching of table
+    public void searchTable(){
+
+    }
+
+    //allows for column to be deleted
+    public void addColumn(String tableName, String columnName, String dataType)throws SQLException{
+
+        if(!columnExist(tableName, columnName)){
+            try{
+                Statement statement = connection.createStatement();
+                String createColumn = "ALTER TABLE " + tableName
+                        + "ADD " +columnName
+                        + " " + dataType; //data type can be attained through user choice.
+
+                statement.execute(createColumn);
+                System.out.println("Table " +tableName+" created");
+            }catch(SQLException e) {
+                e.printStackTrace();
+            }
+        }else{
+            System.out.println("Column " +columnName+ " already exist in" +tableName+ " table");
+        }
+    }
+
+    //allows for column to be added
+    public void deleteColumn(){
+
+    }
+
+}
