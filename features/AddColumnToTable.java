7a580a1f5a87454c31e0110a848d0409d5466ec2
linnypham <89488749+linnypham@users.noreply.github.com>
2024-07-10 19:47:45 -0400

    addcolumntotable

--- features/AddColumnToTable.java
+++ features/AddColumnToTable.java
@@ -1,13 +1,11 @@
-package features;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public void AddColumnToTable(String tableName, String columnName, String dataType) throws SQLException {
     Connection conn = DatabaseConnection.getConnection();
-    PreparedStatement stmt = conn.prepareStatement("ALTER TABLE ? ADD COLUMN ? ?");
-    stmt.setString(1,tableName);
-    stmt.setString(2,columnName);
-    stmt.setString(3, dataType);
-    stmt.executeUpdate();
+    String sql = String.format("ALTER TABLE %s ADD COLUMN %s %s", tableName, columnName, dataType);
+    try (Statement stmt = conn.createStatement()) {
+        stmt.executeUpdate(sql);
+    }
 }
