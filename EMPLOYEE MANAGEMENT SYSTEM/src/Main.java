cc0782551a0952698971344e918dcc86d4e2db76
Marieezouaa <88006867+Marieezouaa@users.noreply.github.com>
2024-07-21 17:36:32 -0400

    Update Main.java

--- EMPLOYEE MANAGEMENT SYSTEM/src/Main.java
+++ EMPLOYEE MANAGEMENT SYSTEM/src/Main.java
@@ -1,36 +1,9 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.Statement;
+import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) {
-        try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/employeedb", "root", "password123456");
+    public static void main(String[] args) throws SQLException {
 
-            Statement statement = connection.createStatement();
+        ClientInteraction.commenceProgram("testEmployeeTable");
 
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM employee;");
-
-            ResultSetMetaData metaData = resultSet.getMetaData();
-            int columnCount = metaData.getColumnCount();
-
-            // Print column names
-            for (int i = 1; i <= columnCount; i++) {
-                System.out.print(metaData.getColumnName(i) + "\t");
-            }
-            System.out.println();
-
-            // Print row data
-            while (resultSet.next()) {
-                for (int i = 1; i <= columnCount; i++) {
-                    System.out.print(resultSet.getString(i) + "\t");
-                }
-                System.out.println();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
