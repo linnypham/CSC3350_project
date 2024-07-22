e5dfe885bba7cf637d0941440b491455874a4950
jamy155 <77408233+jamy155@users.noreply.github.com>
2024-07-21 23:26:39 -0400

    Add files via upload

---
+++ EMPLOYEE MANAGEMENT SYSTEM/src/updateEmployeeSalary.java
@@ -0,0 +1,22 @@
+public String updateEmployeeSalary(Connection conn, Scanner inputScanner)throws SQLException{
+
+        System.out.print("Enter employee ID to update employee's salary : ");
+        int empId = inputScanner.nextInt();
+        System.out.print("Enter salary increase % (EX: 3.2): ");
+        double percentageIncrease = inputScanner.nextDouble();
+        System.out.print("Enter minimal salary: ");
+        int minSalary = inputScanner.nextInt();
+        System.out.print("Enter max  salary: ");
+        int maxSalary = inputScanner.nextInt();
+
+        double multiplier = 1 + (percentageIncrease / 100);
+
+        String sql = String.format("UPDATE employee SET salary = salary * '" + multiplier + "' WHERE empid = '" + empid + "' AND salary BETWEEN '" + minSalary + "' AND '" + maxSalary + "'")
+        try (PreparedStatement updateStatement = conn.prepareStatement(sql)) {
+            updateStatement.setDouble(1,multiplier);
+            updateStatement.setInt(2,empid);
+            updateStatement.setInt(3,minSalary);
+            updateStatement.setInt(4,maxSalary);
+            updateStatement.executeUpdate();
+        }
+}
