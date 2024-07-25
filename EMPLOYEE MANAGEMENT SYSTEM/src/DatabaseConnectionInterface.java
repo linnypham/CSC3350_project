bc224fde5bae9a4537f11bfe5bf6e57add71833a
Marieezouaa <88006867+Marieezouaa@users.noreply.github.com>
2024-07-24 22:29:43 -0400

    Delete EMPLOYEE MANAGEMENT SYSTEM/src directory

--- EMPLOYEE MANAGEMENT SYSTEM/src/DatabaseConnectionInterface.java
+++
@@ -1,7 +0,0 @@
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface DatabaseConnectionInterface {
-    void createConnection() throws SQLException;
-    Connection getConnection();
-}
